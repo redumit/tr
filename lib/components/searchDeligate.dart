4fff3c50d4bde57de480c54401818a0a744de022
Redae <redumit2009@gmail.com>
2020-03-12 10:41:37 +0300

    initial commit

---
+++ lib/components/searchDeligate.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class DataSearch extends SearchDelegate<String>{
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+
+  }
+
+  @override
+  Animation<double> get transitionAnimation {
+    
+  }
+
+  @override
+  void close(BuildContext context, String result) {
+
+  }
+
+  @override
+  void showSuggestions(BuildContext context) {
+
+  }
+
+  @override
+  void showResults(BuildContext context) {
+
+  }
+
+  @override
+  set query(String value) {
+
+  }
+
+  @override
+  String get query {
+
+  }
+
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+
+  }
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+
+  }
+}
